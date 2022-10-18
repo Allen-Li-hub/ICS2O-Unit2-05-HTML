a8dbf4f515a8410c42e5f3bc93991826691d5d89
Allen-Li-hub <Allen-Li-hub@users.noreply.github.com>
2022-10-18 18:18:09 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-'use strict'
+"use strict"
 
-function calculate () {
+function calculate() {
+  const hours = parseInt(
+    document.getElementById("number-of-hours-worked").value
+  )
+  const rate = parseInt(document.getElementById("Hourly-rate:$").value)
 
-  const hours = parseInt(document.getElementById('number-of-hours-worked').value)
-  const rate = parseInt(document.getElementById('Hourly-rate:$').value)
- 
+  const take = hours * rate * 0.18
+  const wage = hours * rate * (1.0 - 0.18)
 
-  const take = (hours * rate)* 0.18
-  const wage = (hours * rate)*(1.00 - 0.18)
-
-
-  document.getElementById('take').innerHTML = 'The governemnt will take: ' + take + '$'
-  document.getElementById('wage').innerHTML = 'your pay will be: ' + wage + '$'
- 
+  document.getElementById("take").innerHTML =
+    "The governemnt will take: " + take + "$"
+  document.getElementById("wage").innerHTML = "your pay will be: " + wage + "$"
 }
