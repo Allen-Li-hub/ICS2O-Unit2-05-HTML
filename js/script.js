0c7acbdfe7f7b8ac46e28f7fcef3440c4281f782
Allen-Li-hub <113055658+Allen-Li-hub@users.noreply.github.com>
2022-10-18 14:15:54 -0400

    change

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function calculate () {
+
+  const hours = parseInt(document.getElementById('number-of-hours-worked').value)
+  const rate = parseInt(document.getElementById('Hourly-rate:$').value)
+ 
+
+  const take = (hours * rate)* 0.18
+  const wage = (hours * rate)*(1.00 - 0.18)
+
+
+  document.getElementById('take').innerHTML = 'The governemnt will take: ' + take + '$'
+  document.getElementById('wage').innerHTML = 'your pay will be: ' + wage + '$'
+ 
+}
